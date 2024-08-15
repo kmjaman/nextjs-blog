bde70864a5fe9d978bb2b057e1bd7eb6c69857ae
Kamrujjaman Md <jaman.edux@gmail.com>
2024-08-15 19:52:38 +0200

    Add post data

---
+++ frontend/_db/data.ts
@@ -0,0 +1,11 @@
+export const posts = [
+    { id: 1, title: 'Post One', content: 'This is a content for post one.', image: '/img/a-01.jpg', category: 'Category 1' },
+    { id: 2, title: 'Post Two', content: 'This is a content for post two.', image: '/img/w-01.jpg', category: 'Category 2' },
+    { id: 3, title: 'Post Three', content: 'This is a content for post three.', image: '/img/t-01.jpg', category: 'Category 3' },
+];
+  
+export const posts_2 = [
+    { id: 1, title: 'Post Four', content: 'This is a content for post four.', image: '/img/f-01.jpg', category: 'Category 1' },
+    { id: 2, title: 'Post Five', content: 'This is a content for post five.', image: '/img/y-01.jpg', category: 'Category 2' },
+    { id: 3, title: 'Post Six', content: 'This is a content for post six.', image: '/img/v-01.jpg', category: 'Category 3' },
+];
