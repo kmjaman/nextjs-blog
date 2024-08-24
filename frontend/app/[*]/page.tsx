c6753faccd257f8e2b0bb0c66ea2a85a4df58c49
Kamrujjaman Md <jaman.edux@gmail.com>
2024-08-24 22:29:50 +0200

    Create PageNotFound

---
+++ frontend/app/[*]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='my-96 flex flex-col items-center'>
+        <p className='font-extrabold'>This page does not exist yet.</p>
+        <p>Please come back later.</p>
+    </div>
+  )
+}
+
+export default PageNotFound
