eebacc86662ca5acc0ea6ca61a17bd1f4fab0393
Kamrujjaman Md <jaman.edux@gmail.com>
2024-10-27 23:20:50 +0100

    Update header background color

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -16,7 +16,7 @@ function classNames(...classes: string[]) {
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-gray-600">
+    <Disclosure as="nav" className="bg-blue-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
