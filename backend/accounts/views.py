35633dbfcaa3cea919cfa08e62092398f7b457cc
Kamrujjaman Md <jaman.edux@gmail.com>
2024-11-01 01:47:23 +0100

    Create user registration and login views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
-# Create your views here.
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            authenticate(username=serializer.data['username'], password=serializer.data['password'])           
+            return Response({"message": "Login success"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
