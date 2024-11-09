24aa55461ace74a2627bbbaba977d1f317b0658a
Kamrujjaman Md <jaman.edux@gmail.com>
2024-11-09 12:59:17 +0100

    Update user login views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
@@ -16,6 +17,12 @@ class UserLoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            authenticate(username=serializer.data['username'], password=serializer.data['password'])           
-            return Response({"message": "Login success"}, status=status.HTTP_200_OK)
+            user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
